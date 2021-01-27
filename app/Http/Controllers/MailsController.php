16d45d2db9b6810f916cbb981c3bf505be60307d
AH3laly <AH3laly@gmail.com>
2021-01-27 22:33:42 +0200

    Implement getStatistics method in Mails Controller

--- app/Http/Controllers/MailsController.php
+++ app/Http/Controllers/MailsController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Mail;
+use App\Models\MTAServer;
+
 class MailsController extends Controller
 {
     public function getStatistics(Request $request){
 
+        // IMPORTANT:
+        // ON Production Servers, Statistics should be collected from cache NOT by hitting the database,
+        // But as it's not a requirement for this test, I am getting statistics by querying the database.
+        return [
+            'totalEmails' => Mail::get()->count(),
+            'deliveredEmails' => Mail::where("status", 1)->get()->count(),
+            'failedEmails' => Mail::where("status", 0)->get()->count(),
+            'activeMTAs' => MTAServer::where("enabled", 1)->get()->count()
+        ];
     }
 
     public function getItems(Request $request){
