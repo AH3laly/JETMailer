bcded13cd60f269fa1f076ffd0eb28e7eeefba97
AH3laly <AH3laly@gmail.com>
2021-01-27 22:05:21 +0200

    Create MailsController and add related Routes

---
+++ app/Http/Controllers/MailsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MailsController extends Controller
+{
+    public function getStatistics(Request $request){
+
+    }
+
+    public function getItems(Request $request){
+
+    }
+
+    public function createItem(Request $request){
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MailsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+/**
+ * Routing for /api/mail forwards requests to DefaultController::class controller
+ */
+Route::group(['prefix'=>'mail'], function(){
+    Route::get('/', [MailsController::class, 'getItems']);
+    Route::get('/statistics', [MailsController::class, 'getStatistics']);
+    Route::get('{id}', [MailsController::class, 'getItem']);
+    Route::post('/', [MailsController::class, 'createItem']);
 });
