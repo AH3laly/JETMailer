a8304976155374f033967320d737574dfa9779d1
AH3laly <AH3laly@gmail.com>
2021-01-27 21:45:51 +0200

    Populate Mail model

--- app/Models/Mail.php
+++ app/Models/Mail.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Mail extends Model
 {
     use HasFactory;
+
+    public $table = 'emails';
+    
+    protected $fillable = [
+        'fromName',
+        'fromEmail',
+        'toEmail',
+        'subject',
+        'body',
+        'status',
+        'format',
+        'isHtml'
+    ];
 }
