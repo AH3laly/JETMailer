6f19aa5f672246b496bab1b0fa055f491dd84fca
AH3laly <AH3laly@gmail.com>
2021-01-27 22:14:50 +0200

    Create JETDelivery class extending the PHPMailer

---
+++ app/Libraries/JETDelivery.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Libraries;
+
+require_once __DIR__.DIRECTORY_SEPARATOR.'PHPMailer'.DIRECTORY_SEPARATOR.'PHPMailer.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'PHPMailer'.DIRECTORY_SEPARATOR.'Exception.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'PHPMailer'.DIRECTORY_SEPARATOR.'SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+class JETDelivery extends PHPMailer
+{
+    // Extend or Override PHPMailer as needed here
+    
+    public function setServerConfig($host, $username, $password, $port = '587', $security = 'tls')
+    {
+        //$mail->SMTPDebug = 3;                               
+        $this->isSMTP();
+        $this->Host = $host;
+        $this->SMTPAuth = true;
+        $this->Username = $username;
+        $this->Password = $password;
+        $this->SMTPSecure = $security;                           
+        $this->Port = $port;
+    }
+
+    public function deliverEmail($message)
+    {
+        $this->FromName = $message['fromName'];
+        $this->From = $message['fromEmail'];
+        $this->addAddress($message['toEmail']);
+        $this->isHTML($message['isHTML']);
+        $this->Subject = $message['subject'];
+        $this->Body = $message['message'];
+        $this->AltBody = $message['subject'];
+        
+        try {
+            if($this->send()){
+                return ["status" => 1, "message" => "Email was Sent"];
+            } else {
+                return ["status" => 0, "message" => $this->ErrorInfo];
+            }
+        } catch (Exception $e) {
+            return ["status" => 0, "message" => $e->getMessage()];
+        }
+    }
+}
