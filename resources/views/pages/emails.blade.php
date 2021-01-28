fd45d6b43864736da8a633ea8b95d5b43d8d2b2a
AH3laly <AH3laly@gmail.com>
2021-01-28 10:36:33 +0200

    Implement Vue on Mails page

--- resources/views/pages/emails.blade.php
+++ resources/views/pages/emails.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.default')
 @section('content')
+
 <!-- Page-Title -->
 <div class="row">
     <div class="col-sm-12">
@@ -14,7 +15,9 @@
         </ol>
     </div>
 </div>
-<div class="row">
+
+@verbatim
+<div class="row" id="app">
     <div class="col-sm-12">
         <div class="card-box">
             <table id="demo-foo-filtering" class="table table-striped toggle-circle m-b-0" data-page-size="7">
@@ -27,41 +30,11 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">From Name <demo@domain.com></a></td>
-                        <td>toemail@domain.com</td>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">Subject</a></td>
-                        <td><span class="label label-table label-success">Status</span></td>
-                    </tr>
-                    <tr>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">From Name <demo@domain.com></a></td>
-                        <td>toemail@domain.com</td>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">Subject</a></td>
-                        <td><span class="label label-table label-danger">Status</span></td>
-                    </tr>
-                    <tr>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">From Name <demo@domain.com></a></td>
-                        <td>toemail@domain.com</td>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">Subject</a></td>
-                        <td><span class="label label-table label-inverse">Status</span></td>
-                    </tr>
-                    <tr>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">From Name <demo@domain.com></a></td>
-                        <td>toemail@domain.com</td>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">Subject</a></td>
-                        <td><span class="label label-table label-success">Status</span></td>
-                    </tr>
-                    <tr>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">From Name <demo@domain.com></a></td>
-                        <td>toemail@domain.com</td>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">Subject</a></td>
-                        <td><span class="label label-table label-danger">Status</span></td>
-                    </tr>
-                    <tr>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">From Name <demo@domain.com></a></td>
-                        <td>toemail@domain.com</td>
-                        <td><a data-toggle="modal" data-target="#myModal" style="cursor:pointer">Subject</a></td>
-                        <td><span class="label label-table label-inverse">Status</span></td>
+                    <tr v-for="item in items">
+                        <td><a data-toggle="modal" data-target="#myModal" v-on:click="viewEmail(item)" style="cursor:pointer">{{item.fromName}} <{{item.fromEmail}}></a></td>
+                        <td>{{item.toEmail}}</td>
+                        <td><a data-toggle="modal" data-target="#myModal" v-on:click="viewEmail(item)" style="cursor:pointer">{{item.subject}}</a></td>
+                        <td><span :class="[item.status==1 ? 'label-success' : 'label-danger']" class="label label-table">Active</span></td>
                     </tr>
                 </tbody>
                 <tfoot>
@@ -69,16 +42,7 @@
                         <td colspan="5">
                             <div class="text-right">
                                     <ul class="pagination pagination-split m-t-30 m-b-0">
-                                    <li class="footable-page-arrow"><a data-page="first" href="#first">«</a></li>
-                                    <li class="footable-page-arrow"><a data-page="prev" href="#prev">‹</a></li>    
-                                    <li class="footable-page active"><a data-page="4" href="#">1</a></li>
-                                    <li class="footable-page active"><a data-page="4" href="#">2</a></li>
-                                    <li class="footable-page active"><a data-page="4" href="#">3</a></li>
-                                    <li class="footable-page active"><a data-page="4" href="#">4</a></li>
-                                    <li class="footable-page active"><a data-page="4" href="#">5</a></li>
-                                    <li class="footable-page active"><a data-page="4" href="#">6</a></li>
-                                    <li class="footable-page-arrow"><a data-page="next" href="#next">›</a></li>
-                                    <li class="footable-page-arrow"><a data-page="last" href="#last">»</a></li>
+                                    <li :class="[page==currentPage ? 'active' : '']" v-on:click="gotoPage(page)" v-for="page in pages" style="cursor:pointer" class="footable-page"><a>{{page}}</a></li>
                                 </ul>
                             </div>
                         </td>
@@ -88,7 +52,7 @@
         </div>
     </div>
 
-    <!-- sample modal content -->
+    <!-- Mail Preview Modal -->
     <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -97,21 +61,64 @@
                     <h4 class="modal-title" id="myModalLabel">Modal Heading</h4>
                 </div>
                 <div class="modal-body">
-                    <h4>Email Subject</h4>
-                    <p>From Name <fromemail@domain.com> <b>To</b> toemail@domain.com</p>
+                    <h4>{{currentItem.subject}}</h4>
+                    <p>From {{currentItem.fromName}} <{{currentItem.fromEmail}}> <b>To</b> {{currentItem.toEmail}}</p>
                     <hr>
-                    <p>Email Body</p>
+                    <p>{{currentItem.message}}</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
                 </div>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
+            </div>
+        </div>
+    </div>
 </div>
+
+@endverbatim
 
 <!--FooTable-->
 <script src="assets/plugins/footable/js/footable.all.min.js"></script>
 <script src="assets/plugins/bootstrap-select/dist/js/bootstrap-select.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+   jQuery(document).ready(function($) {
+    
+        // Activate Current Tab
+        $("#sidebar-menu li").removeClass("active");
+        $("#sidebar-menu li #tab-emails").addClass("active");
+
+        // Vue Implementation
+        var app = new Vue({
+            el: '#app',
+            data: {
+                items: [],
+                itemsCount:0,
+                pages:0,
+                currentPage:1,
+                currentItem: {}
+            },
+            mounted () {
+                this.gotoPage(1);
+            },
+            methods: {
+                gotoPage: function (page) {
+                    axios
+                    .get(baseUrl + '/api/mail/?page=' + page)
+                    .then(response => {
+                        this.items = response.data.items;
+                        this.itemsCount = response.data.itemsCount;
+                        this.pages = response.data.pages;
+                        this.currentPage = response.data.currentPage;
+                    })
+                },
+                viewEmail: function(email){
+                    this.currentItem = email;
+                }
+            }
+        });
+
+    });
+
+</script>
 
 @stop
