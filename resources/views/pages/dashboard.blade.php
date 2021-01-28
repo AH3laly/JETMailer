14f103dc029a9fad51171dcbc868da8328067b8f
AH3laly <AH3laly@gmail.com>
2021-01-28 10:35:36 +0200

    Implement Vue on Dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -9,34 +9,35 @@
     </div>
 </div>
 
-<div class="row">
+@verbatim
+<div class="row" id="app">
     <div class="col-sm-12">
         <div class="card-box widget-inline">
             <div class="row">
                 <div class="col-lg-3 col-sm-6">
                     <div class="widget-inline-box text-center">
-                        <h3><i class="text-primary md md-email"></i> <b>123</b></h3>
+                        <h3><i class="text-primary md md-email"></i> <b>{{stats.totalEmails}}</b></h3>
                         <h4 class="text-muted">Total Emails</h4>
                     </div>
                 </div>
                 
                 <div class="col-lg-3 col-sm-6">
                     <div class="widget-inline-box text-center">
-                        <h3><i class="text-success md md-email"></i> <b>483</b></h3>
+                        <h3><i class="text-success md md-email"></i> <b>{{stats.deliveredEmails}}</b></h3>
                         <h4 class="text-muted">Delivered Emails</h4>
                     </div>
                 </div>
                 
                 <div class="col-lg-3 col-sm-6">
                     <div class="widget-inline-box text-center">
-                        <h3><i class="text-pink md md-block"></i> <b>3494</b></h3>
+                        <h3><i class="text-pink md md-block"></i> <b>{{stats.failedEmails}}</b></h3>
                         <h4 class="text-muted">Failed Emails</h4>
                     </div>
                 </div>
                 
                 <div class="col-lg-3 col-sm-6">
                     <div class="widget-inline-box text-center b-0">
-                        <h3><i class="text-purple md md-flight"></i> <b>3298</b></h3>
+                        <h3><i class="text-purple md md-flight"></i> <b>{{stats.activeMTAs}}</b></h3>
                         <h4 class="text-muted">Active MTAs</h4>
                     </div>
                 </div>
@@ -45,5 +46,30 @@
         </div>
     </div>
 </div>
+@endverbatim
+
+<script type="text/javascript">
+   jQuery(document).ready(function($) {
+    
+        // Activate Current Tab
+        $("#sidebar-menu li").removeClass("active");
+        $("#sidebar-menu li #tab-dashboard").addClass("active");
+
+        // Vue Implementation
+        var app = new Vue({
+            el: '#app',
+            data: {
+                stats: {totalEmails:0, deliveredEmails:0, failedEmails:0, activeMTAs:0}
+            },
+            mounted () {
+                axios
+                .get(baseUrl + '/api/mail/statistics')
+                .then(response => {
+                    this.stats = response.data;
+                })
+            }
+        });
+    });
+</script>
 
 @stop
